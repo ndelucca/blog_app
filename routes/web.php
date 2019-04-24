a20f4f36d25fd359cf39b30f1c4bf02e215fa483
Naza Delucca <nazadelucca@gmail.com>
2019-04-24 16:50:07 -0300

    auth working now

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::fallback('PagesController@fallback');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
