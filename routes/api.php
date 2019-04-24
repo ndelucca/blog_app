f192eaa22a6ee6df5421a5b081107349742bedf0
Naza Delucca <nazadelucca@gmail.com>
2019-04-24 16:12:34 -0300

    routing start

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,3 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-/*
-Route::get('/', 'PagesController@index');
-Route::get('/about', 'PagesController@about');
-Route::get('/services', 'PagesController@services');
-Route::resource('posts','PostsController');
-Route::fallback('PagesController@fallback');
-*/
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+/*
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+Route::resource('posts','PostsController');
+Route::fallback('PagesController@fallback');
+*/
